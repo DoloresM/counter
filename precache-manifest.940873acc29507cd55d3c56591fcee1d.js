36e23389a7862e2023f13375f99e8a053ba1ff87
Dolores <dolores.macauley@gmail.com>
2019-07-18 17:51:30 -0400

    Updates

---
+++ precache-manifest.940873acc29507cd55d3c56591fcee1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbcd935b23ad7cab24f497756d0cb6b",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "8d7275fe65f7549828a3",
+    "url": "/counter/static/css/main.072fc25a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/counter/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8d7275fe65f7549828a3",
+    "url": "/counter/static/js/main.5bb783b8.chunk.js"
+  },
+  {
+    "revision": "395f42efa7c50e4ddc67",
+    "url": "/counter/static/js/runtime~main.16ddd954.js"
+  }
+]);
